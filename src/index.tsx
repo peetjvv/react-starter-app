b95042b54342a98bddadb6c9b3c0a6497e249c45
Peet Janse van Vuuren <peetjvv93@gmail.com>
2023-01-05 17:27:52 +0200

    setup eslint

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,10 @@ import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 import './scss/main.scss';
 
-const App: React.FC<{}> = () => {
+const App: React.FC = () => {
   return <div className="content">Howdy!</div>;
 };
 
 const container = document.getElementById('app');
-const root = createRoot(container!);
+const root = createRoot(container!); // eslint-disable-line @typescript-eslint/no-non-null-assertion
 root.render(<App />);
